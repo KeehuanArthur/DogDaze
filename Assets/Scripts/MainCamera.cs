672dc95aeb02a4a803fb73232807c686e2594932
gksrb32 <54378716+gksrb32@users.noreply.github.com>
2019-10-05 19:46:42 -0500

    Main Camera. Following Target

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.transform.position.y, transform.position.z);
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+    }
+}
