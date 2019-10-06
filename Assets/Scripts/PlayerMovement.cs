3c431ff6b8d759c792b75258a35c4fe256fb8ca5
gksrb32 <54378716+gksrb32@users.noreply.github.com>
2019-10-05 19:48:38 -0500

    Add Weapon option
    Input 1. Use laser
    Input 2. Use tennis ball
    Input 3. Use Booster
    Lock - Rotation.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,98 +15,127 @@ public class PlayerMovement : MonoBehaviour
     SpriteRenderer playerSprite;
 
     [Header("Projectile")] //From here
-    public Rigidbody2D projectile;
-    public float projectileSpeed = 15;
     private Vector3 target; //To here
+    public Rigidbody2D[] Items;
+    Rigidbody2D currentItem;
 
-
-
-/*
-        public float speed = 15;
-    private Vector3 target;
-
-    void Start()
-    {
-        target = transform.position;
-    }
-
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            target.z = transform.position.z;
-        }
-        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-    }
-    */
-
-
+    
 
 
     private void Start()
     {
         playerBody = GetComponent<Rigidbody2D>();
         // playerSprite = GetComponent<SpriteRenderer>();
-        target = projectile.transform.position; // here
+        //target = projectile.transform.position; // here
     }
 
     private void Update()
     {
         PlayerInput();
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
     }
 
     private void FixedUpdate()
     {
         MovePlayer();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetMouseButtonDown(0) && currentItem == Items[0])   // Useful for laser
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Rigidbody2D newCurrentItem;
+            newCurrentItem = Instantiate(currentItem, transform.position, Quaternion.identity);
+            target.z = newCurrentItem.transform.position.z;
+            newCurrentItem.transform.position = target;
+            Destroy(newCurrentItem.gameObject, 3.0f);
+        }
+
+        if (Input.GetMouseButtonDown(0) && currentItem == Items[1])   // Useful for tennisball
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Rigidbody2D newCurrentItem;
+            newCurrentItem = Instantiate(currentItem, transform.position, Quaternion.identity);
+            target.z = newCurrentItem.transform.position.z;
+            newCurrentItem.velocity = newCurrentItem.transform.TransformDirection(target - transform.position);
+            Destroy(newCurrentItem.gameObject, 3.0f);
+        }
+
     }
 
     public void PlayerInput()
     {
+        // PLAYER MOVEMENT
         float hAxis = Input.GetAxisRaw("Horizontal");
         float vAxis = Input.GetAxisRaw("Vertical");
-
         if (hAxis != 0f)
         {
             moveInput = new Vector2(hAxis, 0f);
             if (vAxis != 0f)
                 moveInput = new Vector2(hAxis, vAxis);
         }
-
         else if (vAxis != 0f)
         {
             moveInput = new Vector2(0f, vAxis);
             if (hAxis != 0f)
                 moveInput = new Vector2(hAxis, vAxis);
         }
-
-        else // Infinite movement if this else isnt here....
+        else
+        { // Infinite movement if this else isnt here....
             moveInput = new Vector2(hAxis, vAxis);
-
+        }
         moveVelocity = moveInput.normalized * playerSpeed;
 
 
-        // Projectile movement
-        
-        if (Input.GetMouseButtonDown(0))
+        // ITEM INPUT
+        if (ItemOption() == 1) // Laser
         {
-            
-            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Rigidbody2D newProjectile;
-            newProjectile = Instantiate(projectile, transform.position, Quaternion.identity);
-
-            target.z = newProjectile.transform.position.z;
-            newProjectile.velocity = newProjectile.transform.TransformDirection(target-transform.position);
-            //newProjectile.transform.position = Vector3.MoveTowards(transform.position, target, projectileSpeed * 10);
-            
+            currentItem = Items[ItemOption() - 1];
         }
-        //newProjectile.transform.position = Vector3.MoveTowards(newProjectile.transform.position, target, projectileSpeed * Time.deltaTime);
-
-        //To here
-
+        else if (ItemOption() == 2) // Tennis
+        {
+            currentItem = Items[ItemOption() - 1];
+        }
+        else if (ItemOption() == 3) // Booster
+        {
+            Booster();
+            Invoke("NormalSpeed", 1f);
+        }
     }
-    
 
+    private void NormalSpeed()
+    {
+        playerSpeed = 10;
+    }
+
+    private void Booster()
+    {
+        playerSpeed = 20;
+    }
+
+    public int ItemOption()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            return 1;
+        }
+        else if (Input.GetButton("Fire2"))
+        {
+            return 2;
+        }
+        else if (Input.GetButton("Fire3"))
+        {
+            return 3;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    
     public void MovePlayer()
     {
         playerBody.MovePosition(playerBody.position + moveVelocity * Time.fixedDeltaTime);
